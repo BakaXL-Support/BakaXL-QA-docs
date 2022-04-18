93ad7a289648e473890daa96c5aa0ca20da551f4
XiaoMouz <1114096791@qq.com>
2022-04-18 20:28:46 +0800

    不是很懂

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,8 +3,13 @@ export default ({
     Vue, // VuePress 正在使用的 Vue 构造函数
     options, // 附加到根实例的一些选项
     router, // 当前应用的路由实例
-    siteData // 站点元数据
+    siteData, // 站点元数据
+    isServer 
   }) => {
     window.Vue = vue // 使页面中可以使用Vue构造函数 （使页面中的vue demo生效）
-
+    if(!isServer){
+        import('xxx/lib/xxxx.min.js').then(module => {
+          Vue.use(module)
+        })
+    }    
   }
