78ef4a732599119b5eb5c8ee4f5f5e9b99f4ff73
XiaoMouz <1114096791@qq.com>
2022-04-19 22:17:35 +0800

    重新修改目录结构
    尝试构建

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,22 +12,22 @@ module.exports = {
     // sidebar: 'structuring',
     nav: [
       { text: '主页', link: '/' },
-      { text: '帮助文档', link: '/maindocs/' },
+      { text: '帮助文档', link: '/maindocs/maindocs/' },
       {
         text: '相关文档',
         items: [
-          { text: '1.8-1.16.5版本常见问题', link: '/subdocs/Minecraft_1.8-1.16.5_CRQA/'},
-          { text: '如何提出一个合适的问题', link: '/subdocs/How_To_Asking_Question/' }
+          { text: '1.8-1.16.5版本常见问题', link: '/maindocs/Minecraft_1.8-1.16.5_CRQA/'},
+          { text: '如何提出一个合适的问题', link: '/maindocs/How_To_Asking_Question/' }
         ]
       },
-      { text: '文档编写规则', link: '/subdocs/BakaXL_HelpDocs_WriteRules/' },
+      { text: '文档编写规则', link: '/maindocs/BakaXL_HelpDocs_WriteRules/' },
     ], // 顶部栏配置
     displayAllHeaders: true, // 默认值：false
     sidebarDepth: 2,
     lastUpdated: '最后更新于',
     smoothScroll: true,
     repo: 'https://github.com/BakaXL-Support/BakaXL-QA-docs',
-    repoLabel: '查看文档源',
+    repoLabel: 'Github',
     docsRepo: 'BakaXL-Support/BakaXL-QA-docs',
     docsDir: 'docs',
     docsBranch: 'main',
